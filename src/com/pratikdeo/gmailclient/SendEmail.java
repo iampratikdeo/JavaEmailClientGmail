db0084343d328ddebd9308cc6e17fc9a6355fb9b
Pratik Deo <pratikdeo@Pratiks-MacBook-Pro.local>
2021-06-17 15:59:47 -0400

    email client

---
+++ src/com/pratikdeo/gmailclient/SendEmail.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pratikdeo.gmailclient;
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+/**
+ *
+ * @author pratikdeo
+ */
+public class SendEmail {
+    
+    private String user = "pratikdummy8@gmail.com";
+    private String password = "Pratik@85";
+    
+    SendEmail(String to, String sub, String msg){
+        Properties prop = new Properties();
+        prop.put("mail.smtp.ssl.trust", "smtp.gmail.com");
+        prop.put("mail.smtp.auth", true);
+        prop.put("mail.smtp.starttls.enable", true);
+        prop.put("mail.smtp.host", "smtp.gmail.com");
+        prop.put("mail.smtp.port", "587");
+        
+        Session session = Session.getInstance(prop, new javax.mail.Authenticator() {
+            
+            protected javax.mail.PasswordAuthentication getPasswordAuthentication(){
+                return new javax.mail.PasswordAuthentication(user, password);
+            }
+        });
+        
+        
+        try {
+            
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("no-reply@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to));
+            message.setSubject(sub);
+            message.setText(msg);
+            
+            Transport.send(message);
+            
+            System.out.println("Mail Sent");
+            
+            
+        }catch(Exception e){
+            System.out.println(e);
+        }
+        
+    }
+    
+}
